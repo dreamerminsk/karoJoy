3238170f2b05ea2fb318e0051350887bce2163ec
dKCh <tutmirage@gmail.com>
2019-11-13 21:22:48 +0300

    Create JTagStatsView.java

---
+++ src/cc/joyreactor/views/JTagStatsView.java
@@ -0,0 +1,14 @@
+package cc.joyreactor.views;
+
+public JTagStatsView extends JPanel {
+
+    public JTagStatsView() {
+super(new BorderLayout());
+setupUi();
+    }
+
+    public void setupUi() {
+
+    }
+
+}
