d9a32247c8886fe515108e086b6f7cefff691ee6
KCH <tutmirage@gmail.com>
2019-11-13 13:05:23 +0300

    PostsView

--- src/cc/joyreactor/utils/Strings.java
+++ src/cc/joyreactor/utils/Strings.java
@@ -1,8 +1,16 @@
 package cc.joyreactor.utils;
 
+import com.sun.istack.internal.NotNull;
+
 public class Strings {
 
-
+    public static String getLastSplitComponent(@NotNull String text, @NotNull String regex) {
+        String[] parts = text.split(regex);
+        if (parts.length > 0) {
+            return parts[parts.length - 1];
+        }
+        return null;
+    }
 
 
 }
