0d3d690ca55b5603db7b8912e3490d25eb7b43dd
KCH <tutmirage@gmail.com>
2019-11-15 11:31:15 +0300

    PostsView

--- src/cc/joyreactor/views/PostsView.java
+++ src/cc/joyreactor/views/PostsView.java
@@ -12,6 +12,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
@@ -27,6 +29,7 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -220,7 +223,7 @@ public class PostsView extends JPanel implements PropertyChangeListener {
                     }));
         });
 
-        pubLabel = new JLabel();
+        pubLabel = new JLabel("");
         pubLabel.setFont(pubLabel.getFont().deriveFont(Font.PLAIN | Font.ITALIC, 16.0f));
         pubLabel.addMouseMotionListener(new MouseMotionListener() {
             @Override
@@ -531,4 +534,126 @@ public class PostsView extends JPanel implements PropertyChangeListener {
     public void propertyChange(PropertyChangeEvent evt) {
 
     }
+
+
+    public static class FadeOutLabel extends JLabel {
+        private static final int LENGTH = 20;
+        private static final float DIFF = .05f;
+
+        protected FadeOutLabel(String text) {
+            super(text);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            Insets i = getInsets();
+            int w = getWidth() - i.left - i.right;
+            int h = getHeight() - i.top - i.bottom;
+
+            Rectangle rect = new Rectangle(i.left, i.top, w - LENGTH, h);
+
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setFont(g.getFont());
+            g2.setClip(rect);
+            g2.setComposite(AlphaComposite.SrcOver.derive(.99f));
+            super.paintComponent(g2);
+
+            rect.width = 1;
+            float alpha = 1f;
+            for (int x = w - LENGTH; x < w; x++) {
+                rect.x = x;
+                alpha = Math.max(0f, alpha - DIFF);
+                g2.setComposite(AlphaComposite.SrcOver.derive(alpha));
+                g2.setClip(rect);
+                super.paintComponent(g2);
+            }
+            g2.dispose();
+        }
+    }
+
+    static class TextOverflowFadeLabel extends JLabel {
+        private static final int LENGTH = 20;
+        private static final float DIFF = .05f;
+
+        protected TextOverflowFadeLabel(String text) {
+            super(text);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            Insets i = getInsets();
+            int w = getWidth() - i.left - i.right;
+            int h = getHeight() - i.top - i.bottom;
+            Rectangle rect = new Rectangle(i.left, i.top, w - LENGTH, h);
+
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setFont(g.getFont());
+            g2.setPaint(getForeground());
+
+            FontRenderContext frc = g2.getFontRenderContext();
+            TextLayout tl = new TextLayout(getText(), getFont(), frc);
+            int baseline = getBaseline(w, h);
+
+            g2.setClip(rect);
+            tl.draw(g2, i.left, baseline);
+
+            rect.width = 1;
+            float alpha = 1f;
+            for (int x = w - LENGTH; x < w; x++) {
+                rect.x = x;
+                alpha = Math.max(0f, alpha - DIFF);
+                g2.setComposite(AlphaComposite.SrcOver.derive(alpha));
+                g2.setClip(rect);
+                tl.draw(g2, i.left, baseline);
+            }
+            g2.dispose();
+        }
+    }
+
+    static class FadingOutLabel extends JLabel {
+        private static final int LENGTH = 20;
+        private final Dimension dim = new Dimension();
+        private transient Image buffer;
+
+        protected FadingOutLabel(String text) {
+            super(text);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            // super.paintComponent(g);
+            int w = getWidth();
+            int h = getHeight();
+            if (Objects.isNull(buffer) || dim.width != w || dim.height != h) {
+                dim.setSize(w, h);
+                buffer = updateImage(dim);
+            }
+            g.drawImage(buffer, 0, 0, this);
+        }
+
+        private BufferedImage updateImage(Dimension d) {
+            BufferedImage img = new BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = img.createGraphics();
+            g2.setFont(getFont());
+            g2.setPaint(getForeground());
+            FontRenderContext frc = g2.getFontRenderContext();
+            TextLayout tl = new TextLayout(getText(), getFont(), frc);
+            int baseline = getBaseline(d.width, d.height);
+            tl.draw(g2, getInsets().left, baseline);
+            g2.dispose();
+
+            int spx = Math.max(0, d.width - LENGTH);
+            for (int x = 0; x < LENGTH; x++) {
+                double factor = 1d - x / (double) LENGTH;
+                for (int y = 0; y < d.height; y++) {
+                    int argb = img.getRGB(spx + x, y);
+                    int rgb = argb & 0x00_FF_FF_FF;
+                    int a = (argb >> 24) & 0xFF;
+                    img.setRGB(spx + x, y, ((int) (a * factor) << 24) | rgb);
+                }
+            }
+            return img;
+        }
+    }
+
 }
