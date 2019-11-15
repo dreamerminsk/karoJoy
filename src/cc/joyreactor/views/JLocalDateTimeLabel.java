0e2d6c03227c754fa77048aa4d7538ab1c4e3a5e
KCH <tutmirage@gmail.com>
2019-11-15 20:12:10 +0300

    PostsView

--- src/cc/joyreactor/views/JLocalDateTimeLabel.java
+++ src/cc/joyreactor/views/JLocalDateTimeLabel.java
@@ -2,8 +2,6 @@ package cc.joyreactor.views;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.HashMap;
-import java.util.stream.IntStream;
 
 public class JLocalDateTimeLabel extends JLabel {
 
@@ -22,46 +20,33 @@ public class JLocalDateTimeLabel extends JLabel {
     }
 
     protected void paintComponent(Graphics g) {
-        // Get text-contents of Label
         String text = this.getText();
 
-        // No text in the JLabel? -> No risk: super
         if (text == null || text.length() == 0) {
             super.paintComponent(g);
             return;
         }
 
-        // Content Array of characters to paint
         char[] chars = text.toCharArray();
 
-        // Draw nice and smooth
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-        // Draw background
         if (this.isOpaque()) {
             g2d.setColor(this.getBackground());
             g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
         }
 
-        // FontMetrics to calculate widths and height
         FontMetrics fm = g2d.getFontMetrics();
 
-        // Available space
         Insets ins = this.getInsets();
-        int maxSpace = this.getWidth() - (ins.left + ins.right);
-        boolean overflow = (fm.stringWidth(text) > maxSpace);
 
-        // Starting offset
         int offset = ins.left + 1;
 
-        // The start Color is the default
         g2d.setColor(this.getForeground());
 
-        // Loop over characters
         for (int i = 0; i < chars.length; i++) {
-            // Switch Color?
-            if (i >= selectedStart || i < (selectedStart + selectedLength)) {
+            if (i >= selectedStart && i < (selectedStart + selectedLength)) {
                 g2d.setColor(extraColor);
             } else {
                 g2d.setColor(this.getForeground());
@@ -69,7 +54,6 @@ public class JLocalDateTimeLabel extends JLabel {
 
             g2d.drawString(String.valueOf(chars[i]), offset, (fm.getHeight() + ins.top));
 
-            // Move cursor to the next horizontal position
             offset += fm.charWidth(chars[i]);
         }
     }
