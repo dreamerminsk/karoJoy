36620f5636cfe983fe3ce7bac95e017d55641ca0
KCH <tutmirage@gmail.com>
2019-11-17 22:33:37 +0300

    PostsView

--- src/cc/joyreactor/Source.java
+++ src/cc/joyreactor/Source.java
@@ -40,7 +40,7 @@ public class Source {
 
     public List<Tag> getTags() {
         List<Tag> tags = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM jr_tags WHERE ids LIKE '%,%';")) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM jr_tags;")) {
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
                     tags.add(new Tag(
